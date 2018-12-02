b7ba1fadfbcada790aa26a8a96848df77766a0ff
Mike James <m@mikejam.es>
2018-12-02 22:08:20 +0000

    updates

--- webapp/440111/src/App.js
+++ webapp/440111/src/App.js
@@ -10,7 +10,9 @@ class App extends Component {
     number: '',
     error: '',
     newNumber: '',
-    password: localStorage.getItem('token') || ''
+    name: '',
+    password: localStorage.getItem('token') || '',
+    numbers: []
   }
   componentDidMount() {
     this.fetch();
@@ -40,13 +42,15 @@ class App extends Component {
     }
   }
   onSubmit = async() => {
+    const newNumber = this.state.newNumber.trim().replace(/\s/g,'');
     if (
-      this.state.newNumber &&
-      this.state.newNumber.startsWith('07') &&
-      this.state.newNumber.length === 11
+      newNumber &&
+      newNumber.startsWith('07') &&
+      newNumber.length === 11
     ) {
       try {
         this.setState({
+          error: false,
           validationError: false,
           submitting: true
         });
@@ -54,14 +58,16 @@ class App extends Component {
           method: 'POST',
           body: JSON.stringify({
             token: this.state.password,
-            number: this.state.newNumber
+            number: newNumber
           })
         })
         if (!res.ok) throw Error('error');
         localStorage.setItem('token', this.state.password);
         await this.fetch();
+        const numbers = this.state.numbers.filter(n => n.number !== newNumber);
         this.setState({
-          submitting: false
+          submitting: false,
+          numbers: [...numbers, {number: newNumber, name: this.state.name}]
         });
       } catch (e) {
         this.setState({
@@ -103,6 +109,10 @@ class App extends Component {
                 <label for="first_name">Password</label>
               </div>
               <div className="row">
+                <input placeholder="Drivers Name" id="name" value={this.state.name} type="text" onChange={this.onChange('name')}/>
+                <label for="name">Name</label>
+              </div>
+              <div className="row">
                 <input placeholder="New Number" id="newNumber" value={this.state.newNumber} type="number" onChange={this.onChange('newNumber')}/>
                 <label for="newNumber">Change redirect to</label>
               </div>
@@ -112,6 +122,32 @@ class App extends Component {
                 </a>
               </div>
             </form>
+            <ul className="collection">
+              {this.state.numbers.map(n => <li key={n.number} className="collection-item">
+                <div>
+                  {n.name}
+                  <a href="#!" className="secondary-content">
+                    <i className="material-icons" onClick={() => {
+                      this.setState({
+                        name: n.name,
+                        newNumber: n.number
+                      }, () => {
+                        this.onSubmit();
+                      })
+                    }}>
+                      use
+                    </i>
+                    <i style={{marginLeft: 10, color: 'red'}} className="material-icons" onClick={() => {
+                      this.setState({
+                        numbers: this.state.numbers.filter(m => m.number !== n.number)
+                      })
+                    }}>
+                      remove
+                    </i>
+                  </a>
+                </div>
+              </li>)}
+            </ul>
           </div>
         </div>}
       </div>
